3e5b7c30f33a008221b66548a84a5478864fac85
err-himanshu <himanshu10.gupta@ril.com>
2018-08-08 13:40:30 +0530

    project config changes

---
+++ config/autoreload.js
@@ -0,0 +1,31 @@
+if(process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'production'){
+module.exports.autoreload = {
+  active: false,
+  usePolling: false,
+  dirs: [
+    "api/models",
+    "api/controllers",
+    "api/services",
+    "config/locales"
+  ],
+  ignored: [
+    // Ignore all files with .ts extension
+    "**.ts"
+  ]
+};
+}else{
+  module.exports.autoreload = {
+    active: true,
+    usePolling: false,
+    dirs: [
+      "api/models",
+      "api/controllers",
+      "api/services",
+      "config/locales"
+    ],
+    ignored: [
+      // Ignore all files with .ts extension
+      "**.ts"
+    ]
+  };
+}
---
+++ config/bootstrap.js
@@ -0,0 +1,47 @@
+/**
+ * Bootstrap
+ * (sails.config.bootstrap)
+ *
+ * An asynchronous bootstrap function that runs just before your Sails app gets lifted.
+ * > Need more flexibility?  You can also do this by creating a hook.
+ *
+ * For more information on bootstrapping your app, check out:
+ * https://sailsjs.com/config/bootstrap
+ */
+
+module.exports.bootstrap = async function(done) {
+
+  // By convention, this is a good place to set up fake data during development.
+  //
+  // For example:
+  // ```
+  // // Set up fake development data (or if we already have some, avast)
+  // if (await User.count() > 0) {
+  //   return done();
+  // }
+  //
+  // await User.createEach([
+  //   { emailAddress: 'ry@example.com', fullName: 'Ryan Dahl', },
+  //   { emailAddress: 'rachael@example.com', fullName: 'Rachael Shaw', },
+  //   // etc.
+  // ]);
+  // ```
+
+  // Don't forget to trigger `done()` when this bootstrap function's logic is finished.
+  // (otherwise your server will never lift, since it's waiting on the bootstrap)
+
+  //catching all the uncaught exception here
+
+  sails.moment = require('moment');
+  process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+    process.exit(1);
+  });
+
+  return done();
+
+};
